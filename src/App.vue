ec97f7332a4b77f60c92d4d0441926eb75a730ef
Mafi518 <xolluej@list.ru>
2021-06-04 17:34:58 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav id="nav" class="menu">
+    <div class="menu__social">
+      <Icon name="navigation" class="menu__navigation first-level-icon"></Icon>
+      <a href="" class="facebook-icon menu__network"
+        ><Icon class="first-level-icon" name="facebook"></Icon
+      ></a>
+      <a href="" class="instagram-icon menu__network"
+        ><Icon class="first-level-icon" name="instagram"></Icon
+      ></a>
+      <a href="" class="vk-icon menu__network"
+        ><Icon class="" name="vk"></Icon
+      ></a>
+    </div>
+    <div class="menu__info">
+      <div class="menu__location">
+        <Icon name="location" class="second-level-icon"></Icon>
+        <span
+          >Санкт-Петербург <br />
+          Москва</span
+        >
+      </div>
+      <div class="menu__pho">
+        <Icon name="phone" class="second-level-icon"></Icon>
+        <span
+          >22 3333 333 9 <br />
+          22 3333 333 9</span
+        >
+      </div>
+      <Icon name="logo" class="logo second-level-icon"></Icon>
+    </div>
+  </nav>
   <router-view />
 </template>
 
+<script>
+import Icon from "@/components/Icon";
+
+export default {
+  name: "App",
+  components: {
+    Icon,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "@/assets/styles/normalize.scss";
+body {
+  max-height: 100vh;
+  height: 100vh;
 }
-
 #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  z-index: 9999;
+}
+.menu {
+  position: absolute;
+  width: 100%;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  &__social {
+    display: flex;
+    justify-content: space-between;
+    max-width: 30vw;
+    width: 100%;
+  }
+  &__network {
+    margin: 0 20px;
+  }
+  &__navigation {
+    min-width: 48px;
+    min-height: 48px;
+  }
+  &__info {
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="home" id="home">
+    <div class="home__left">
+      <div class="home__plate">
+        <div class="home__plate-1"></div>
+        <div class="home__plate-2"></div>
+        <div class="home__product">
+          <img src="@/assets/images/set-1.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div class="home__right"></div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  justify-content: space-between;
+  &__left {
+    background-color: $accent;
+    max-width: 33vw;
+    width: 100%;
+    max-height: 100vh;
+    min-height: 100vh;
+    position: relative;
+  }
+  &__right {
+    max-width: 47vw;
+    width: 100%;
+    max-height: 100vh;
+    min-height: 100vh;
+  }
+  &__plate {
+    width: 500px;
+    height: 500px;
+    position: absolute;
+    top: 50%;
+    left: 100%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &__plate-1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: $accent;
+    width: 50vh;
+    height: 50vh;
+    border-radius: 50%;
+    border: 1px solid rgba(0, 0, 0, 0.09);
+    z-index: -1;
+  }
+  &__plate-2 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: $accent;
+    width: 48vh;
+    height: 48vh;
+    border-radius: 50%;
+    box-shadow: inset 0px 0px 8px rgba(0, 0, 0, 0.25);
+    z-index: -1;
+  }
+  &__product {
+    max-width: 430px;
+  }
+}
+</style>
