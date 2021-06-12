c66d21e8d8560f41f27df3cf5920458564494fed
Mafi518 <xolluej@list.ru>
2021-06-12 15:00:58 +0300

    showFullInfo

--- src/components/Fullcard.vue
+++ src/components/Fullcard.vue
@@ -2,7 +2,8 @@
   <article class="description">
     <h1 class="description__title">Сет “Глазурь”</h1>
     <p class="description__subtitle">
-      <slot></slot>
+      Пышные пончики с сладкой глазурью со вкусом клубники и шоколада,
+      посыпанные крошкой – сытный десерт для вкусного перекуса!
     </p>
     <button class="description__structure">Состав</button>
     <div class="description__buy">
